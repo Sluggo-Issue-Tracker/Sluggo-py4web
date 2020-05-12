97263c0b4355f08edb2c31e5397507b9db3c713b
Isaac Trimble-Pederson <wtpisaac@icloud.com>
2020-05-11 17:35:41 -0700

    First pass at database items Credit tdimhcsleumas for ticket_rels design

--- models.py
+++ models.py
@@ -13,3 +13,43 @@ from pydal.validators import *
 #
 # db.commit()
 #
+
+def get_user_email():
+    return auth.current_user.get('email')
+
+# TODO: Do we want separate projects to be included in the db?
+
+db.define_table(
+    'users',
+    Field('first_name'),
+    Field('last_name'),
+    Field('user_email'),
+    Field('role')
+)
+
+db.define_table(
+    'ticket',
+    Field('title'),
+    Field('due_date'), # TODO find date format + serialize
+    Field('description')
+
+    # Additional properties that must be queried:
+    # Dependenencies -> select all children from ticket_rels
+    # Dependents -> select all parents from ticket_rels
+    # Percentage -> BFS and give proportion (should cache longterm)
+)
+
+db.define_table( # credit tdimhcsleumas for design
+    'ticket_rels',
+    Field('parent', 'reference ticket'),
+    Field('child', 'reference ticket')
+)
+
+# TODO tags, roles, other fun things that require relationships
+
+# TODO readables vs not readables (relevant? can we even use default forms?)
+# TODO requirements for forms (again, is this even relevant?)
+
+db.ticket_rels.ondelete = 'NO ACTION' # We don't want relationships to affect tickets
+
+db.commit()
