534f72cd7c875b059efcd66254f7886935828b91
Andrew Gavgavian <andrewgee7@gmail.com>
2020-05-17 17:08:00 -0700

    Added user page and loading users dynamically from server

--- controllers.py
+++ controllers.py
@@ -24,13 +24,17 @@ The path follows the bottlepy syntax.
 session, db, T, auth, and tempates are examples of Fixtures.
 Warning: Fixtures MUST be declared with @action.uses({fixtures}) else your app will result in undefined behavior
 """
+import base64
+import pathlib
+import uuid
+
 
 from py4web import action, request, abort, redirect, URL
 from yatl.helpers import A
 from . common import db, session, T, cache, auth, signed_url
 from . models import get_user_email, get_user_title, get_user_name
 
-
+# --------------------------------------------------- TICKETS --------------------------------------------------- #
 @action('index')
 @action.uses('index.html', signed_url, auth.user)
 def index():
@@ -73,3 +77,58 @@ def delete_tickets():
     if id is not None:
         db(db.tickets.id == id).delete()
         return "ok"
+
+
+# --------------------------------------------------- USERS --------------------------------------------------- #
+
+USERS = [ {"first_name": "Andrew", "last_name": "Gavgavian", "user_email" : "agavgavi@ucsc.edu", "role": "Administrator", "bio":"Hi mom!"},
+          {"first_name": "Isaac", "last_name": "Trimble-Pederson", "user_email" : "itrimble@ucsc.edu", "role": "Administrator", "bio":"Test Bio"},
+          {"first_name": "Samuel", "last_name": "Schmidt", "user_email" : "sadschmi@ucsc.edu", "role": "Systems", "bio":"Shit"}]
+
+@action('users/setup')
+@action.uses( auth.user, db)
+def setup():
+    db(db.users).delete()
+    for u in USERS:
+        db.users.insert(first_name=u.get('first_name'),
+                        last_name=u.get('last_name'),
+                        user_email=u.get('user_email'),
+                        role=u.get('role'),
+                        bio=u.get('bio'))
+    return "ok"
+
+@action('users')
+@action.uses('users.html', signed_url, auth.user)
+def users():
+    return dict(
+        get_users_url = URL('users/get_users', signer=signed_url),
+        get_icons_url = URL('users/get_icons', signer=signed_url),
+        user_email = get_user_email(),
+        username = get_user_title(),
+        user=auth.get_user()
+    )
+
+
+@action('users/get_users')
+@action.uses(signed_url.verify(), auth.user)
+def get_users():
+    users = db(db.users).select().as_list()
+
+    for user in users:
+        user["icon"] = "%s-%s.jpg" % (user.get('first_name').lower(), user.get('last_name').lower())
+        user["full_name"] = "%s %s" % (user.get('first_name'), user.get('last_name'))
+    return dict(users=users)
+
+
+@action('users/get_icons')
+@action.uses(signed_url.verify())
+def get_img():
+    """Returns a single image, URL encoded."""
+    # Reads the image.
+    img_name = request.params.img
+    img_file = pathlib.Path(__file__).resolve().parent / 'static' / 'images' / img_name
+    with img_file.open(mode='rb') as f:
+        img_bytes = f.read()
+        b64_image = base64.b64encode(img_bytes).decode('utf-8')
+    # Returns the image bytes, base64 encoded, and with the correct prefix.
+    return dict(imgbytes="data:image/jpeg;base64," + b64_image)
---
+++ static/js/users.js
@@ -0,0 +1,85 @@
+// This will be the object that will contain the Vue attributes
+// and be used to initialize it.
+let app = {};
+
+// Given an empty app object, initializes it filling its attributes,
+// creates a Vue instance, and then initializes the Vue instance.
+let init = (app) => {
+
+    // This is the Vue data.
+    app.data = {
+        user_email: user_email,
+        username: username,
+        users: [],
+        page: 'list',
+
+        // Complete.
+    };
+    // Use this function to reindex the posts, when you get them, and when
+    // you add / delete one of them.
+    app.reindex = (a) => {
+        let idx = 0;
+        for (p of a) {
+            p._idx = idx++;
+        }
+        return a;
+    };
+
+    app.goto = (destination) => {
+        app.vue.page = destination;
+        // app.vue.add_post_text = "";
+    };
+
+    // We form the dictionary of all methods, so we can assign them
+    // to the Vue app in a single blow.
+    app.methods = {
+        goto: app.goto,
+
+    };
+
+    // This creates the Vue instance.
+    app.vue = new Vue({
+        el: "#vue-users",
+        data: app.data,
+        methods: app.methods
+    });
+
+  app.init = () => {
+        axios.get(get_users_url).then((result) => {
+            var user_promises = [];
+            let users = result.data.users;
+            app.reindex(users);
+            for (let user of users) {
+                // We create an element in the images data structure.
+                // Note: it is SUPER important here to have the url attribute
+                // of img_el already defined.
+                let user_el = user;
+                app.vue.users.push(user_el);
+                // We create a promise for when the image loads.
+                let p = axios.get(
+                    get_icon_url,
+                    {params: {"img": user["icon"]}}).then((result) => {
+                    // Puts the image URL.
+                    // See https://vuejs.org/v2/guide/reactivity.html#For-Objects
+                    Vue.set(user_el, 'url', result.data.imgbytes);
+                    return "ok";
+                });
+                user_promises.push(p);
+            }
+            Promise.all(user_promises).then((r) => {
+                    app.vue.done = "All done";
+                    console.log(r);
+            });
+        });
+    };
+
+
+
+
+    // Call to the initializer.
+    app.init();
+};
+
+// This takes the (empty) app object, and initializes it,
+// putting all the code i
+init(app);
