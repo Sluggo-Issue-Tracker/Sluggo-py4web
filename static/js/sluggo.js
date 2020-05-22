8af0c1c733677c0db7ee61e876ebf78464ed7ef8
Isaac Trimble-Pederson <wtpisaac@icloud.com>
2020-05-22 16:31:55 -0700

    moved the period out of the common date formatting

--- static/js/sluggo.js
+++ static/js/sluggo.js
@@ -2,8 +2,7 @@ sluggo = {}
 
 sluggo.formatDate = (date) => { // consistent date formatting
     return date.toLocaleDateString('default', {weekday: 'long'})
-    + ", " + date.toLocaleDateString('default', {month: 'long', day: 'numeric', year: 'numeric'})
-    + ".";
+    + ", " + date.toLocaleDateString('default', {month: 'long', day: 'numeric', year: 'numeric'});
 }
 
 sluggo.placeholder = () => {
