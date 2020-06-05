d24b8490f6336a7152407e36294e4318b5f82a0c
Isaac Trimble-Pederson <wtpisaac@icloud.com>
2020-06-05 08:03:16 -0700

    Basic priority fetching logic

--- controllers/index.py
+++ controllers/index.py
@@ -37,6 +37,11 @@ def tickets():
     # Grab user tags
     user_tags = Helper.get_web_tag_list_for_user_id(Helper.get_user())
 
+    # Grab priority tickets
+    user_id = Helper.get_our_user_id(Helper.get_user())
+    priority_tickets = Helper.get_priority_ticket_ids_for_user(user_id)
+    print(priority_tickets)
+
     return(dict(
         user_email=Helper.get_user_email(),
         username=Helper.get_user_title(),
@@ -45,6 +50,7 @@ def tickets():
         ticket_details_url = URL('ticket_details'),
         tickets_url = URL('tickets'),
         pinned_tickets = Helper.safe_json_dumps(pinned_tickets),
+        priority_tickets = Helper.safe_json_dumps(priority_tickets),
         user_tags = Helper.safe_json_dumps(user_tags)
     ))
 
