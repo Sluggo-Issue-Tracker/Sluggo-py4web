3c32f5dbd42fb7779dca2d0aea77ef914e663dfa
Isaac Trimble-Pederson <wtpisaac@icloud.com>
2020-06-11 15:35:38 -0700

    Removed old mockup for proposal (not needed)

--- doc/mockup/__init__.py
+++
@@ -1,8 +0,0 @@
-# __init__.py - initializer for mockup app
-# Copyright (c) 2020 Andrew Gavgavian, Samuel Schmidt, and Isaac
-# Trimble-Pederson, All Rights Reserved
-
-# This imports the controllers.
-from . import controllers
-
-
